a416b274882df1f84e20423a61a06ccf590508a1
juliannaroen <julianna.roen@gusto.com>
2019-07-03 16:28:08 -0700

    Include classes in `lib/password_police.rb`

--- lib/password_police.rb
+++ lib/password_police.rb
@@ -1,8 +1,3 @@
-# frozen_string_literal: true
-
 require 'password_police/version'
-
-module PasswordPolice
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require 'password_police/calculate_strength'
+require 'password_police/role_to_password_strength_map'
