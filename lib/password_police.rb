56f03e44a567318df3831132949543a06c0c3aa4
juliannaroen <julianna.roen@gusto.com>
2019-07-03 17:01:34 -0700

    Cast strings to integers as input

--- lib/password_police.rb
+++ lib/password_police.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'password_police/version'
 require 'password_police/calculate_strength'
 require 'password_police/role_to_password_strength_map'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
+
 require 'bundler/setup'
 require 'password_police'
 require 'rspec'
-
